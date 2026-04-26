6c659cd0d2f4aa04eb90d32183c16f531b78642b
Bar Weiss <73524722+barweiss1@users.noreply.github.com>
2026-04-26 13:24:22 +0300

    Update logo_funcs.py

--- helper_functions/logo_funcs.py
+++ helper_functions/logo_funcs.py
@@ -202,7 +202,7 @@ def process_iteration(data_dict, sim_params, validation_idx, figures_path, bias_
     font_properties_ticks = sim_params['font_properties_ticks']
     angles_common = lin2circ_angles(data_dict[f'common_angles'])
     results = []
-    for scale in tqdm.tqdm(sim_params['scales']):
+    for scale in tqdm.tqdm(sim_params['scales'], desc=f'Processing scales'):
         start_time = time()
         A1, _, _ = Create_Asym_Tran_Kernel(s1_aligned, s1_ref, mode='median', scale=scale)
         A2, _, _ = Create_Asym_Tran_Kernel(s2_aligned, s2_ref, mode='median', scale=scale)
